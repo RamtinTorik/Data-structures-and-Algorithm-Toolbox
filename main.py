5d3e793ee4cd4c9019c15fc0d7f57de97a469c9e
ramtintorik <rotmari101@gmail.com>
2024-01-03 20:01:01 +0330

    complete case 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def print_menu():
     os.system("cls")
@@ -29,6 +30,7 @@ def return_menu():
             os.system("cls")
             print("Please enter correct key :)\n")
             
+            
 def enter_array():
     arr = [int(i) for i in list(input("Please enter your array: ").split(','))]
     return arr
@@ -56,9 +58,35 @@ def binary_search():
 
 def insertion_sort():
     arr = enter_array()
-        
+    n=len(arr)
+    print("\n", end ='')
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+        while (arr[j] > key) & (j>=0):
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+        print(f"step {i}: ", arr)
+    print("\nsorted array: ", arr)
+    
+    
 def selection_sort():
     arr = enter_array()
+    n=len(arr)
+    print("\n", end ='')
+    for i in range(n-1, 0, -1):
+        # print(i)
+        max = arr[0]
+        index = 0
+        for j in range(1, i+1):
+            if(arr[j]>max):
+                max = arr[j]
+                index = j
+        arr[index] = arr[i]
+        arr[i] = max
+        print(f"step {n-i}: ", arr)
+    print("\nsorted array: ", arr)
 
 def sort_array():
     while True:
@@ -68,11 +96,16 @@ def sort_array():
         myinput = input("Which type of sorting do you choose? ")
         if myinput == '1':
             insertion_sort()
+            break
         elif myinput == '2':
-            arr = enter_array()
+            selection_sort()
+            break
         else:
             print("Please enter correct number :)")
         
+def infix_converter():
+    stack = list()
+    
     
 def main():
     while True:
@@ -88,8 +121,6 @@ def main():
 
             case '2':
                 os.system("cls")
-                
-                print("case2")
                 sort_array()
                 return_menu()
                 
@@ -119,6 +150,13 @@ def main():
                 return_menu()
                 
 main()
+
+# selection_sort()
+# arr = enter_array()
+
+# binary_search(arr)
+# bubblesort(arr)
+# }
 # print(binary_search())
 
 
