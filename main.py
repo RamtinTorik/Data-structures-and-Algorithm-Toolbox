5164d3da24e385671024a84fdca3eea65e59a20f
ramtintorik <rotmari101@gmail.com>
2024-01-04 22:57:20 +0330

    complete case 3: infix to prefix and postfix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import regex
 
 def print_menu():
     os.system("cls")
@@ -104,7 +105,90 @@ def sort_array():
             print("Please enter correct number :)")
         
 def infix_converter():
-    stack = list()
+    
+    while True:
+        mystring = input("Please enter your infix string: ")
+        result = regex.match(r"^(([+-]*\d+|\((?1)\))([+^*/-](?2))*)$", mystring)
+        if "None" in str(result):
+            os.system("cls")
+            print("please enter infix string ;)")
+        else:
+            break
+            
+    def prec(c):
+        if c == '^':
+            return 3
+        elif c == '/' or c == '*':
+            return 2
+        elif c == '+' or c == '-':
+            return 1
+        else:
+            return -1
+
+    def associativity(c):
+        if c == '^':
+            return 'R'
+        return 'L' # Default to left-associative
+    
+    def infix_to_postfix(infix):
+        result = []
+        stack = []
+
+        for i in range(len(infix)):
+            c = infix[i]
+
+            # If the scanned character is an operand, add it to the output string.
+            if ('a' <= c <= 'z') or ('A' <= c <= 'Z') or ('0' <= c <= '9'):
+                result.append(c)
+            # If the scanned character is an ‘(‘, push it to the stack.
+            elif c == '(':
+                stack.append(c)
+            # If the scanned character is an ‘)’, pop and add to the output string from the stack
+            # until an ‘(‘ is encountered.
+            elif c == ')':
+                while stack and stack[-1] != '(':
+                    result.append(stack.pop())
+                stack.pop() # Pop '('
+            # If an operator is scanned
+            else:
+                while stack and (prec(infix[i]) < prec(stack[-1]) or
+                                (prec(infix[i]) == prec(stack[-1]) and associativity(infix[i]) == 'L')):
+                    result.append(stack.pop())
+                stack.append(c)
+
+        # Pop all the remaining elements from the stack
+        while stack:
+            result.append(stack.pop())
+
+        postfix = ''.join(result)
+        return postfix
+    
+    def infix_to_prefix(infix):
+        l = len(infix)
+    
+        infix = infix[::-1]
+    
+        l_infix = []
+        
+        for i in range(l):
+            l_infix.append(infix[i]) 
+                
+        for i in range(l):
+            if l_infix[i] == '(':
+                l_infix[i] = ')'
+            elif l_infix[i] == ')':
+                l_infix[i] = '('
+
+        infix = ''.join(l_infix)
+        
+        prefix = infix_to_postfix(infix)
+        prefix = prefix[::-1]
+    
+        return prefix
+    
+    print("prefix:", infix_to_prefix(mystring))
+    print("postfix:",infix_to_postfix(mystring))
+    # print(infix_to_postfix(mystring))
     
     
 def main():
@@ -126,7 +210,7 @@ def main():
                 
             case '3':
                 os.system("cls")
-                print("case3")
+                infix_converter()
                 return_menu()
             
             case '4':
@@ -145,8 +229,7 @@ def main():
                 exit(1)
             case _:
                 os.system("cls")
-                print(
-                "Please enter correct number :)")
+                print("Please enter correct number :)")
                 return_menu()
                 
 main()
